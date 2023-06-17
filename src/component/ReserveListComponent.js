0efab93f96bea189bf82c65eb4e5e869cd19f7d0
VictoryJu <kob0805@naver.com>
2023-06-18 01:02:33 +0900

    feat: 예약목록 퇴석 버튼 클릭시 리스트 삭제 기능 구현

--- src/component/ReserveListComponent.js
+++ src/component/ReserveListComponent.js
@@ -4,11 +4,14 @@ import { timeFormat } from "../utils/timeFormat.js";
 export class ReserveListComponent {
     constructor (reserveDatas,app){
         this.app = app;
-        this.render(reserveDatas);
+        this.initialDatas = reserveDatas.reservations;
+        this.render(this.initialDatas);
     }
 
+    initialDatas
+
     render(data){
-        const result = data.reservations.filter(item=>item.status!=="done").map((item)=>{
+        const result = data.filter(item=>item.status!=="done").map((item)=>{
             const tables = item.tables.map((table)=>table.name).join(", ");
             const menus = item.menus.map((menu)=>`${menu.name}(${menu.qty})`).join(", ");
             const registered = timeFormat(item.timeRegistered);
@@ -23,11 +26,31 @@ export class ReserveListComponent {
                         <div class="type">성인 ${item.customer.adult} 아이 ${item.customer.child}</div>
                         <div class="menu">${menus}</div>
                     </div>
-                    <button class="reserve-btn">${statusType[item.status].btnText}</button>
+                    <button data-type="${item.status}" class="reserve-btn">${statusType[item.status].btnText}</button>
                 </div>
             `
         });
         this.app.innerHTML = result.join("");
+        this.setClickBtns()
     }
+
+
+    reserveBtnClicked (type,id) {
+        if(type==='seated') this.deleteReserve(id);
+    }
+
+    deleteReserve(id){
+        this.initialDatas = this.initialDatas.filter(item => item.id !== id);
+        this.render(this.initialDatas);
+    }
+
+    setClickBtns = () => {
+            const buttons = document.getElementsByClassName("reserve-btn");
+            Array.prototype.forEach.call(buttons,(button)=>{
+                button.addEventListener("click",(el)=>{
+                    this.reserveBtnClicked(el.target.dataset.type,el.target.parentElement.id);
+                })
+            })
+        }
     
 }
