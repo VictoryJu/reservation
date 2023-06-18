2284b9952d1a7563bf02d626f4a7b9df1a60dcf7
VictoryJu <kob0805@naver.com>
2023-06-18 17:14:40 +0900

    feat: 예약 리스트 외의 다른곳 클릭시 상세 예약 닫는 outSideClick 함수 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App {
     constructor(app){
         this.app = app;
         this.render();
+        this.outSideClick();
     }
     async render() {
         try {
@@ -16,6 +17,19 @@ class App {
         } catch(e) {
             console.log(e);
         }
+    }
+
+    outSideClick(){
+        document.addEventListener("click", function (event) {
+            const targetEl = event.target;
+            const containerEl = document.querySelector("main");
+            if (!containerEl.contains(targetEl)) {
+              const detailEl = document.querySelector(".reservation-detail-container");
+              detailEl.style.display = "none";
+              return;
+            }
+          });
+          return
     }
 }
 export default App;
