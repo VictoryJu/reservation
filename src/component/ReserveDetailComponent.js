04a5ca46ff3e5d8bbc71821b971f927fa4a0e4a3
VictoryJu <kob0805@naver.com>
2023-06-18 22:28:59 +0900

    feat: mobile 상세 팝업 닫기버튼 추가 && refact: dimEl 상수로 관리

--- src/component/ReserveDetailComponent.js
+++ src/component/ReserveDetailComponent.js
@@ -1,27 +1,33 @@
 import { statusType } from "../data/statusType.js";
 import { timeFormat } from "../utils/timeFormat.js";
 
+const DIM_EL = document.querySelector(".dim");
+
 export class ReserveDetailComponent {
     constructor (reserveData,app){
         this.app = app;
         this.initialData = reserveData[0];
         this.render(this.initialData);
         this.outSideClick();
+        this.closeBtnClick();
     }
 
     initialData
+    
     render(reserve){
-        const dimEl = document.querySelector(".dim");
         this.app.style.display = "block"
-        dimEl.style.display = "block"
+        DIM_EL.style.display = "block"
         const {timeRegistered,timeReserved,status} = reserve;
         const {name,level,memo,request} = reserve.customer;
         const registeredTime = timeFormat(timeRegistered);
         const reservedTime = timeFormat(timeReserved);
         const result = `
         <div class="detail-wrap">
-            <div class="title">예약 정보</div>
-                <div class="info-line">
+            <div class="title">
+                예약 정보
+                <div class="close-btn">닫기</div>
+            </div>
+            <div class="info-line">
                 <div class="left-item">예약 상태</div>
                 <div class="right-item">${statusType[status].description}</div>
             </div>
@@ -35,7 +41,7 @@ export class ReserveDetailComponent {
             </div>
         </div>
         <div class="customer-wrap">
-            <div class="title">고객 정보</div>
+            <div class="title customer">고객 정보</div>
             <div class="info-line">
                 <div class="left-item">고객 성명</div>
                 <div class="right-item name">${name}</div>
@@ -59,16 +65,23 @@ export class ReserveDetailComponent {
         this.app.innerHTML = result
     }
 
+    closeBtnClick(){
+        const closeEl = document.querySelector(".close-btn");
+        closeEl.addEventListener("click",()=>{
+            this.app.style.display = "none";
+            DIM_EL.style.display = "none"
+            return
+        })
+        return
+    }
+
     outSideClick(){
-        document.addEventListener("click", function (event) {
+        document.addEventListener("click", (event) => {
             const targetEl = event.target;
-            const containerEl = document.querySelector(".dim");
-            if (containerEl.contains(targetEl)) {
-              const detailEl = document.querySelector(".reservation-detail-container")
-              const dimEl = document.querySelector(".dim");
-              detailEl.style.display = "none";
-              dimEl.style.display = "none";
-              return;
+            if (DIM_EL.contains(targetEl)) {
+              this.app.style.display = "none";
+              DIM_EL.style.display = "none";
+              return
             }
           });
           return
